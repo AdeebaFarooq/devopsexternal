dec0c392b87b3de804bbb85f88e6b7fe2e88c952
adeebafarooq <adeebafarooq39@gmail.com>
2025-01-03 20:25:16 +0530

    added

---
+++ double.java
@@ -0,0 +1,12 @@
+public class double {
+    public static void main(String[] args) {
+        // Define the number
+        int num = 6;
+
+        // Double the number
+        int doubled = num * 2;
+
+        // Output the result
+        System.out.println("The double of " + num + " is: " + doubled);
+    }
+}
